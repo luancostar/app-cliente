5e2d6b980f2eee841fc2c2bbeba9459ccb802883
Moises Sousa <moises.sousa583@gmail.com>
2023-04-18 11:10:13 -0300

    att

--- pages/chat/php/get-chat.php
+++ pages/chat/php/get-chat.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (isset($_SESSION['logado'])) {
+if (isset($_SESSION['cliente_logado'])) {
     include_once "../../../conexao.php";
     $outgoing_id = $_SESSION['unique_id'];
     $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
